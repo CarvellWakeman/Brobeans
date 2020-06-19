106a8677481773f3e0a598f5027ee617358578c6
carvellwakeman <carvellwakeman@gmail.com>
2020-06-18 18:24:27 -0700

    init

---
+++ Brobeans1/Objects/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace Brobeans1.Objects
+{
+	/// <summary>
+	/// Base of something else, can't have one alone
+	/// </summary>
+	public abstract class Car
+	{
+		public int Wheels;
+
+		public Car(int wheels)
+		{
+			Wheels = wheels;
+		}
+	}
+
+	public class Sedan : Car
+	{
+		public int Doors { get; }
+
+		public Sedan(int doors, int wheels) : base(wheels)
+		{
+			Doors = doors;
+		}
+	}
+
+	public class Pickup : Car
+	{
+		public bool TruckNuts { get; }
+
+		public Pickup(int wheels, bool truckNuts = true) : base(wheels)
+		{
+			TruckNuts = truckNuts;
+		}
+	}
+}
---
+++ Brobeans1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Brobeans1.Objects;
+using System;
+
+namespace Brobeans1
+{
+	/// <summary>
+	/// NOTES:
+	/// OOP / classes / instances
+	/// Namespaces
+	/// Exceptions
+	/// String format sugar
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Read/write console
+			//Console.WriteLine("What is your name? ");
+
+			//var name = Console.ReadLine();
+			//Console.WriteLine($"Hello {name}!");
+			#endregion
+
+			#region Environment variables
+			Console.WriteLine(Environment.GetEnvironmentVariable("MY_VAR"));
+			#endregion
+
+			#region OOP
+			var myCar = new Sedan(2, 5);
+			Console.WriteLine(myCar.Wheels); // 0 when Wheels is not set
+
+			var myTruck = new Pickup(6, false);
+			if (myTruck.TruckNuts)
+			{
+				Console.WriteLine("My Truck Has Nuts: yes");
+			}
+			else
+			{
+				Console.WriteLine("My Truck Has Nuts: no");
+			}
+			#endregion
+		}
+	}
+}
